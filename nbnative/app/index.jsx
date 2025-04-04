da1625fc3a49dd13c5fb73b53bde0687ec43b598
Kabir-Sial <sial059689@gmail.com>
2025-04-04 23:35:53 +0500

    navigation working

--- nbnative/app/index.jsx
+++ nbnative/app/index.jsx
@@ -1,10 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from '../src/screens/Home'
+
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <StackContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={Home} />
+
+      </Stack.Navigator>
+    </StackContainer>
+  );
+};
+
+
 
 const index = () => {
   return (
+
     <View>
-      <Text>index</Text>
+
     </View>
   )
 }
