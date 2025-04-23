853104939d8ce3511bad64f8292ea88cbdb6b935
Kabir-Sial <sial059689@gmail.com>
2025-04-23 22:41:56 +0500

    home page working

--- Expensify_App/app/screens/Home.jsx
+++ Expensify_App/app/screens/Home.jsx
@@ -1,15 +1,38 @@
-import { StyleSheet, Text, View, Pressable, Button } from 'react-native'
+import { StyleSheet, Text, View, Pressable, Button, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import Search from './Search'
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
   return (
-    <View>
-      <Button title="Search" onPress={() => navigation.navigate("Search")}></Button>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text style={styles.mainHeadingText}>Expensify</Text>
+        <Pressable>
+          <Text onPress={() => navigation.navigate("Search")}>Logout</Text>
+        </Pressable>
+
+      </View>
+
+      <View>
+        <Image style={styles.mainImg} source={require('../src/images/inventory.png')} />
+      </View>
+
+    </SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainHeadingText: {
+    fontSize: 30
+  },
+  mainImg:{
+    flex: 1,
+    justifyContent: 'center',
+    width:360,
+    height:500,
+    resizeMode: 'stretch',
+    
+  }
+})
